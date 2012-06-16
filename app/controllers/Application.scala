434f77116f8a93053b3bf9e33fafc6f0eba1743b
Pere Villega <pere.villega@gmail.com>
2012-06-16 16:38:04 +0100

    Fix caching issues
    Due to conflicts between caching of the UI and Pjax, I've enabled pjax again and removed the UI calls from the cache. This should solve the problems.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -22,21 +22,13 @@ object Application extends Controller {
   def index = Action {
     implicit request =>
       Logger.info("Application.index accessed")
-      val render = Cache.getOrElse("index", cacheStorage) {
-        Logger.info("Application.index not in cache")
-        val projects = Project.homeList()
-        val bio = Bio.getBio()
-        val posts = Post.homeList()
-        val quotes = Quote.all()
-        Logger.debug("Application.index - projects [%s] bio [%s]".format(projects, bio))
-        views.html.index(projects, posts, bio, quotes)
-      }
-      Ok(render)
+      val projects = Project.homeList()
+      val bio = Bio.getBio()
+      val posts = Post.homeList()
+      val quotes = Quote.all()
+      Logger.debug("Application.index - projects [%s] bio [%s]".format(projects, bio))
+      Ok(views.html.index(projects, posts, bio, quotes))
   }
-
-
-  //TODO: review texts!
-  //TODO: verify that restart in heroku cleans cache
 
   /**
    * Shows the links stored to check later
@@ -44,14 +36,10 @@ object Application extends Controller {
   def archive() = Action {
     implicit request =>
       Logger.info("Application.archive accessed")
-      val render = Cache.getOrElse("archive", cacheStorage) {
-        Logger.info("Application.archive not in cache")
-        val unchecked = Link.getAllUnchecked()
-        val archived = Link.getArchivedLinks()
-        Logger.debug("Application.archive - to show archived[%s] unchecked[%s]".format(archived, unchecked))
-        views.html.application.archive(archived, unchecked)
-      }
-      Ok(render)
+      val unchecked = Link.getAllUnchecked()
+      val archived = Link.getArchivedLinks()
+      Logger.debug("Application.archive - to show archived[%s] unchecked[%s]".format(archived, unchecked))
+      Ok(views.html.application.archive(archived, unchecked))
   }
 
   /**
@@ -60,13 +48,9 @@ object Application extends Controller {
   def projects() = Action {
     implicit request =>
       Logger.info("Application.projects accessed")
-      val render = Cache.getOrElse("projects", cacheStorage) {
-        Logger.info("Application.projects not in cache")
-        val projects = Project.all()
-        Logger.debug("Application.projects - projects to show [%s]".format(projects))
-        views.html.application.projects(projects)
-      }
-      Ok(render)
+      val projects = Project.all()
+      Logger.debug("Application.projects - projects to show [%s]".format(projects))
+      Ok(views.html.application.projects(projects))
   }
 
   /**
@@ -75,13 +59,9 @@ object Application extends Controller {
   def fullBio() = Action {
     implicit request =>
       Logger.info("Application.fullBio accessed")
-      val render = Cache.getOrElse("fullBio", cacheStorage) {
-        Logger.info("Application.fullBio not in cache")
-        val bio = Bio.getFullBio()
-        Logger.debug("Application.fullBio - fullBio to show [%s]".format(bio))
-        views.html.application.fullbio(bio)
-      }
-      Ok(render)
+      val bio = Bio.getFullBio()
+      Logger.debug("Application.fullBio - fullBio to show [%s]".format(bio))
+      Ok(views.html.application.fullbio(bio))
   }
 
   /**
@@ -91,13 +71,9 @@ object Application extends Controller {
   def blog(page: Int) = Action {
     implicit request =>
       Logger.info("Application.blog accessed for page[%d]".format(page))
-      val render = Cache.getOrElse("blog" + page, cacheStorage) {
-        Logger.info("Application.blog for page[%d] not in cache".format(page))
-        val (posts, previous, next) = Post.page(page)
-        Logger.debug("Application.blog - posts to show [%s]".format(posts))
-        views.html.application.posts(posts, previous, next, page)
-      }
-      Ok(render)
+      val (posts, previous, next) = Post.page(page)
+      Logger.debug("Application.blog - posts to show [%s]".format(posts))
+      Ok(views.html.application.posts(posts, previous, next, page))
   }
 
   /**
@@ -108,13 +84,9 @@ object Application extends Controller {
   def tagged(tag: String, page: Int) = Action {
     implicit request =>
       Logger.info("Application.tagged accessed for tag[%s] page[%d]".format(tag, page))
-      val render = Cache.getOrElse("tagApp" + page + tag, cacheStorage) {
-        Logger.info("Application.tagged for tag[%s] page[%d] not in cache".format(tag, page))
-        val (posts, previous, next) = Post.tagged(tag, page)
-        Logger.debug("Application.tagged - posts to show [%s]".format(posts))
-        views.html.application.tagged(posts, previous, next, page, tag)
-      }
-      Ok(render)
+      val (posts, previous, next) = Post.tagged(tag, page)
+      Logger.debug("Application.tagged - posts to show [%s]".format(posts))
+      Ok(views.html.application.tagged(posts, previous, next, page, tag))
   }
 
   /**
@@ -129,22 +101,15 @@ object Application extends Controller {
   def post(id: Int, slug: String, year: String, month: String, day: String) = Action {
     implicit request =>
       Logger.info("Application.post accessed for id[%d] slug[%s][%s-%s-%s]".format(id, slug, year, month, day))
-      val render = Cache.getOrElse("postApp" + id + year + month + day, cacheStorage) {
-        Logger.info("Application.post for id[%d] slug[%s][%s-%s-%s] not in cache".format(id, slug, year, month, day))
-        Post.getById(id) match {
-          case Some(post) => {
-            Logger.debug("Application.post - post to show [%s]".format(post))
-            Some(views.html.application.post(post))
-          }
-          case _ => {
-            Logger.warn("Application.post - post [%d] not found".format(id))
-            None
-          }
+      Post.getById(id) match {
+        case Some(post) => {
+          Logger.debug("Application.post - post to show [%s]".format(post))
+          Ok(views.html.application.post(post))
         }
-      }
-      render match {
-        case Some(r) => Ok(r)
-        case _ => NotFound(views.html.errors.error404(request.path))
+        case _ => {
+          Logger.warn("Application.post - post [%d] not found".format(id))
+          NotFound(views.html.errors.error404(request.path))
+        }
       }
   }
 
